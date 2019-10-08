235d94712558a84cd82e9249ac3d4b3ee9233d6f
HyeonjaeKim <now97kim@naver.com>
2019-10-08 11:51:36 +0900

    #12 상점 index 구현 중, 이미지 추가, 링크 추가, 카테고리 선택, 검색 기능 추가 필요

--- WebContent/shop/index.jsp
+++ WebContent/shop/index.jsp
@@ -1,10 +1,35 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<%@ page import = "Shop.Shop" %>
+<%@ page import = "Bean.Shop" %>
+<%@ page import = "Bean.ShopBean" %>
+<%@ page import = "java.util.List" %>
+<%@ page import = "java.text.NumberFormat" %>
 <%@ include file="/assets/include/login_check.jsp" %>
+<%!
+    int pageSize = 6;
+%>
 <%
 	Shop sh = new Shop();
 	int point = sh.getPoint(email);
+	
+	String admin = "admin@plare.cf";
+	
+	String pageNum = request.getParameter("pageNum");
+    if (pageNum == null) {
+        pageNum = "1";
+    }
+	String category = request.getParameter("category");
+	if (category == null) {
+		category = "all";
+	}
+	int currentPage = Integer.parseInt(pageNum);
+    int startRow = (currentPage - 1) * pageSize + 1;
+    int endRow = currentPage * pageSize;
+    int count = 0;
+    
+    Shop shop = Shop.getInstance();
+	List<ShopBean> itemList = shop.getItems(category, startRow, pageSize);
+	count = shop.getItemCount(category);
 %>
 <!DOCTYPE HTML>
 <html>
@@ -22,45 +47,68 @@
 								<h4>내포인트 : <%=point %></h4>
 								<h3>스킨</h3><br>
 							</header>
+<%
+	if(email.equals(admin)) {
+%>
+		<a href="write.jsp" class="button special pull-right">상품등록</a><br><br>
+<% 
+	}
+%>
 							<div class="row">
+<%
+	for(int i=0; i<itemList.size(); i++) {
+		ShopBean item = itemList.get(i);
+%>
 							  <div class="col-md-4">
-							  <a href="#">
-							  <img src="/assets/images/test2.jpg" width="220px" height="220px">
-							  <p align="center">이름</p>
-							  <p align="center">가격</p>
-							  </a></div>
-							  <div class="col-md-4">
-								<a href="#">
-								<img src="/assets/images/test2.jpg" width="220px" height="220px">
-								<p align="center">이름</p>
-								<p align="center">가격</p>
-							  </a></div>
-							  <div class="col-md-4">
-								<a href="#">
-								<img src="/assets/images/test2.jpg" width="220px" height="220px">
-								<p align="center">이름</p>
-								<p align="center">가격</p>
-							  </a></div>
+								  <img src="/assets/images/test2.jpg" width="220px" height="220px"><br><br>
+								  <p align="center"><%=item.getItem_name() %></p>
+								  <p align="center" style="line-height:20px; vertical-align:middle;"><img src="/assets/images/PointLogo.png" height="16px;"> 
+								  	<%=NumberFormat.getInstance().format(item.getItem_price()) %>
+								  </p>
+							  </div>
+<%
+		if(i==2) {
+%>
 							</div>
 							<div class="row">
-							  <div class="col-md-4">
-							  <a href="#">
-							  <img src="/assets/images/test2.jpg" width="220px" height="220px">
-							  <p align="center">이름</p>
-							  <p align="center">가격</p>
-							  </a></div>
-							  <div class="col-md-4">
-								<a href="#">
-								<img src="/assets/images/test2.jpg" width="220px" height="220px">
-								<p align="center">이름</p>
-								<p align="center">가격</p>
-							  </a></div>
-							  <div class="col-md-4">
-								<a href="#">
-								<img src="/assets/images/test2.jpg" width="220px" height="220px">
-								<p align="center">이름</p>
-								<p align="center">가격</p>
-							  </a></div>
+<%
+		}
+	}
+%>
+							</div><br>
+							<div class="row">
+								<div class="align-center" style="width:100%;">
+<%
+    if (count > 0) {
+        int pageCount = count / pageSize + (count % pageSize == 0 ? 0 : 1);
+		int startPage = 1;
+		
+		if(currentPage % 5 != 0)
+           startPage = (int)(currentPage/5)*5 + 1;
+		else
+           startPage = ((int)(currentPage/5)-1)*5 + 1;
+		int pageBlock = 5;
+        int endPage = startPage + pageBlock - 1;
+        if (endPage > pageCount) endPage = pageCount;
+        
+        String href = "index.jsp?pageNum=";
+        
+        if (startPage > 5) { 
+%>
+        	<a href="<%=href %><%= startPage - 5 %>" id="link">&lt;</a>
+<%      }
+        
+        for (int i = startPage ; i <= endPage ; i++) {  %>
+        	<a href="<%=href %><%= i %>" id="link" <%if (i == currentPage) {%> style="font-weight:bold; color:#ff0000;"<% } %>>[<%= i %>]</a>
+<%      }
+        
+        if (endPage < pageCount) {  %>
+   	     	<a href="<%=href %><%= startPage + 5 %>" id="link">&gt;</a>
+<%
+        }
+    }
+%>
+								</div>
 							</div>
 						</div>
 					</div>
---
+++ src/Bean/Shop.java
@@ -0,0 +1,283 @@
+package Bean;
+
+import java.util.*;
+import java.sql.*;
+import javax.sql.*;
+import javax.naming.*;
+import Bean.Database;
+import Bean.ShopBean;
+
+public class Shop {
+	private static Shop instance = new Shop();
+    //.jsp페이지에서 DB연동빈인 BoardBean클래스의 메소드에 접근시 필요
+    public static Shop getInstance() {
+        return instance;
+    }	
+	
+	//포인트조회
+	public int getPoint(String email) {
+		Connection conn = Database.connect();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		int point = 0;
+
+		try {
+			String sql = "call store_selectPoint(?)";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, email);
+			rs = pstmt.executeQuery();
+			
+			rs.next();
+			point = rs.getInt("credits");
+		} catch(Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			if (rs != null) try { rs.close(); } catch(SQLException ex) {}
+			if (pstmt != null) try { pstmt.close(); } catch(SQLException ex) {}
+			if (conn != null) try { conn.close(); } catch(SQLException ex) {}
+		}
+		return point;	//해당이메일 플레이어의 현재포인트
+	}
+	
+	//포인트 추가+
+	public void plusPoint(String email, int ppoint) {
+		Connection conn = Database.connect();
+		PreparedStatement pstmt = null;
+		
+		try {
+			String sql = "call store_plusPoint(?,?)";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, email);
+			pstmt.setInt(2, ppoint);
+			pstmt.executeQuery();
+		} catch(Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			if (pstmt != null) try { pstmt.close(); } catch(SQLException ex) {}
+			if (conn != null) try { conn.close(); } catch(SQLException ex) {}
+		}
+	}
+	
+	//아이템구매 이메일,품목아이디
+	public int buyItem(String email, String mid) {
+		Connection conn = Database.connect();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		int error = 3;
+		
+		try {
+			String sql = "call store_buyItem(?,?)";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, email);
+			pstmt.setString(2, mid);
+			rs = pstmt.executeQuery();
+			
+			rs.next();
+			error = rs.getInt("error");
+		} catch(Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			if (rs != null) try { rs.close(); } catch(SQLException ex) {}
+			if (pstmt != null) try { pstmt.close(); } catch(SQLException ex) {}
+			if (conn != null) try { conn.close(); } catch(SQLException ex) {}
+		}
+		return error;	//0=성공, 1=포인트부족, 2=이미구매한아이템, 3=DB연결실패
+	}
+	
+	public int getItemCount(String category) {
+		// 전체 아이템 개수를 가져오는 함수
+		Connection conn = Database.connect();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		int count = 0;
+		
+		try {
+			if(category.equals("all")) {
+				pstmt = conn.prepareStatement("select count(*) from store_menu");
+				rs = pstmt.executeQuery();
+				
+				if(rs.next()) {
+					count = rs.getInt(1);
+				}
+			} else if(category.equals("skin")) {
+				pstmt = conn.prepareStatement("select count(*) from store_menu where item_type = playerskin");
+				rs = pstmt.executeQuery();
+				
+				if(rs.next()) {
+					count = rs.getInt(1);
+				}
+			} else if(category.equals("weapon")) {
+				pstmt = conn.prepareStatement("select count(*) from store_menu where item_type = weapon");
+				rs = pstmt.executeQuery();
+				
+				if(rs.next()) {
+					count = rs.getInt(1);
+				}
+			}
+		} catch(Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			if (rs != null) try { rs.close(); } catch(SQLException ex) {}
+			if (pstmt != null) try { pstmt.close(); } catch(SQLException ex) {}
+			if (conn != null) try { conn.close(); } catch(SQLException ex) {}
+		}
+		return count;	
+	}
+	
+	public List<ShopBean> getItems(String category, int start, int end) {
+		Connection conn = Database.connect();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		List<ShopBean> itemList = null;
+		
+		try {
+			if(category.equals("all")) {
+				pstmt = conn.prepareStatement(
+						"select * from store_menu order by id desc limit ?, ?");
+				pstmt.setInt(1, start-1);
+				pstmt.setInt(2, end);
+				rs = pstmt.executeQuery();
+				
+				if(rs.next()) {
+					itemList = new ArrayList<ShopBean>(end);
+					do {
+						ShopBean item = new ShopBean();
+						item.setId(rs.getInt("id"));
+						item.setUnique_id(rs.getString("unique_id"));
+						item.setItem_type(rs.getString("item_type"));
+						item.setItem_name(rs.getString("item_name"));
+						item.setItem_price(rs.getInt("item_price"));
+						item.setItem_count(rs.getInt("item_count"));
+						item.setItem_cat(rs.getString("item_cat"));
+						item.setItem_dec(rs.getString("item_dec"));
+						
+						itemList.add(item);
+					} while(rs.next());
+				}
+			} else if(category.equals("skin")) {
+				pstmt = conn.prepareStatement(
+						"select * from store_menu where item_type = playerskin order by id desc limit ?, ?");
+				pstmt.setInt(1, start-1);
+				pstmt.setInt(2, end);
+				rs = pstmt.executeQuery();
+				
+				if(rs.next()) {
+					itemList = new ArrayList<ShopBean>(end);
+					do {
+						ShopBean item = new ShopBean();
+						item.setId(rs.getInt("id"));
+						item.setUnique_id(rs.getString("unique_id"));
+						item.setItem_type(rs.getString("item_type"));
+						item.setItem_name(rs.getString("item_name"));
+						item.setItem_price(rs.getInt("item_price"));
+						item.setItem_count(rs.getInt("item_count"));
+						item.setItem_cat(rs.getString("item_cat"));
+						item.setItem_dec(rs.getString("item_dec"));
+						
+						itemList.add(item);
+					} while(rs.next());
+				}
+			} else if(category.equals("weapon")) {
+				pstmt = conn.prepareStatement(
+						"select * from store_menu where item_type = weapon order by id desc limit ?, ?");
+				pstmt.setInt(1, start-1);
+				pstmt.setInt(2, end);
+				rs = pstmt.executeQuery();
+				
+				if(rs.next()) {
+					itemList = new ArrayList<ShopBean>(end);
+					do {
+						ShopBean item = new ShopBean();
+						item.setId(rs.getInt("id"));
+						item.setUnique_id(rs.getString("unique_id"));
+						item.setItem_type(rs.getString("item_type"));
+						item.setItem_name(rs.getString("item_name"));
+						item.setItem_price(rs.getInt("item_price"));
+						item.setItem_count(rs.getInt("item_count"));
+						item.setItem_cat(rs.getString("item_cat"));
+						item.setItem_dec(rs.getString("item_dec"));
+						
+						itemList.add(item);
+					} while(rs.next());
+				}
+			}
+		} catch(Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			if (rs != null) try { rs.close(); } catch(SQLException ex) {}
+			if (pstmt != null) try { pstmt.close(); } catch(SQLException ex) {}
+			if (conn != null) try { conn.close(); } catch(SQLException ex) {}
+		}
+		return itemList;
+	}
+	
+	public ShopBean getItem(String category, int num) {
+		Connection conn = Database.connect();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		ShopBean item = null;
+		
+		try {
+			if(category.equals("all")) {
+				pstmt = conn.prepareStatement(
+						"select * from store_menu where id = ?");
+				pstmt.setInt(1, num);
+				rs = pstmt.executeQuery();
+				
+				if(rs.next()) {
+					item = new ShopBean();
+					item.setId(rs.getInt("id"));
+					item.setUnique_id(rs.getString("unique_id"));
+					item.setItem_type(rs.getString("item_type"));
+					item.setItem_name(rs.getString("item_name"));
+					item.setItem_price(rs.getInt("item_price"));
+					item.setItem_count(rs.getInt("item_count"));
+					item.setItem_cat(rs.getString("item_cat"));
+					item.setItem_dec(rs.getString("item_dec"));
+				}
+			} else if(category.equals("skin")) {
+				pstmt = conn.prepareStatement(
+						"select * from store_menu where id = ? and item_type = playerskin");
+				pstmt.setInt(1, num);
+				rs = pstmt.executeQuery();
+				
+				if(rs.next()) {
+					item = new ShopBean();
+					item.setId(rs.getInt("id"));
+					item.setUnique_id(rs.getString("unique_id"));
+					item.setItem_type(rs.getString("item_type"));
+					item.setItem_name(rs.getString("item_name"));
+					item.setItem_price(rs.getInt("item_price"));
+					item.setItem_count(rs.getInt("item_count"));
+					item.setItem_cat(rs.getString("item_cat"));
+					item.setItem_dec(rs.getString("item_dec"));
+				}
+			} else if(category.equals("weapon")) {
+				pstmt = conn.prepareStatement(
+						"select * from store_menu where id = ? and item_type = weapon");
+				pstmt.setInt(1, num);
+				rs = pstmt.executeQuery();
+				
+				if(rs.next()) {
+					item = new ShopBean();
+					item.setId(rs.getInt("id"));
+					item.setUnique_id(rs.getString("unique_id"));
+					item.setItem_type(rs.getString("item_type"));
+					item.setItem_name(rs.getString("item_name"));
+					item.setItem_price(rs.getInt("item_price"));
+					item.setItem_count(rs.getInt("item_count"));
+					item.setItem_cat(rs.getString("item_cat"));
+					item.setItem_dec(rs.getString("item_dec"));
+				}
+			}
+		} catch(Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			if (rs != null) try { rs.close(); } catch(SQLException ex) {}
+			if (pstmt != null) try { pstmt.close(); } catch(SQLException ex) {}
+			if (conn != null) try { conn.close(); } catch(SQLException ex) {}
+		}
+		return item;
+	}
+}

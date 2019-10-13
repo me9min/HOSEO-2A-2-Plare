b6800c839e97ea3304768ced609cf891da60ada6
user <user@user>
2019-10-13 19:44:50 +0900

    카카오로그인 토큰,아이디 받아오기구현 및 상점품목이미지추가

--- WebContent/member/login/kakao_login.jsp
+++ WebContent/member/login/kakao_login.jsp
@@ -1,11 +1,49 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<%@ page import="java.sql.*,Kakao.KakaoLogin" %>
+<%@ page import="java.sql.*,java.util.*,Kakao.KakaoLogin"%>
+<%@ page import="com.google.gson.*,com.google.gson.internal.LinkedTreeMap"%>
 <%
-	KakaoLogin KakaoLogin = new KakaoLogin();
+// 	String email = (String)session.getAttribute("email");
+// 	if(email == null) {
+// 		return;
+// 	}
+	
+	Gson gson = new Gson();
+	KakaoLogin kakao = new KakaoLogin();
 	String code = request.getParameter("code");
 	
-	out.println(code);
-	String hc = KakaoLogin.kakaoHttpPost(code);
-	out.println(hc);
+	String json_profile = kakao.getProfile(kakao.getToken(code));
+	
+	Map<String,String> json2 = new HashMap<String,String>();
+	json2 = (Map<String,String>) gson.fromJson(json_profile, json2.getClass());
+	Map<String,String> json3 = new HashMap<String,String>();
+	json3 = (Map<String,String>) gson.fromJson(json2.get("properties"), json3.getClass());
 %>
+
+<pre><%=code %></pre>
+<pre><%=json_profile %></pre>
+
+
+
+<br><br><br><br><br>
+<pre>
+{
+	"id":1188766878,
+	"properties":
+	{
+		"nickname":"박지호",
+		"profile_image":"http://k.kakaocdn.net/dn/brVKnv/btqy0qL38Pu/T1NlUWER9Mt8hHA9nKjKv0/profile_640x640s.jpg",
+		"thumbnail_image":"http://k.kakaocdn.net/dn/brVKnv/btqy0qL38Pu/T1NlUWER9Mt8hHA9nKjKv0/profile_110x110c.jpg"
+	},
+	"kakao_account":
+	{
+		"profile_needs_agreement":false,
+		"profile":
+		{
+			"nickname":"박지호",
+			"thumbnail_image_url":"http://k.kakaocdn.net/dn/ccEO2c/btqwMUJzxhm/RsYgf63B2o3bsmp7at09Vk/img_110x110.jpg",
+			"profile_image_url":"http://k.kakaocdn.net/dn/ccEO2c/btqwMUJzxhm/RsYgf63B2o3bsmp7at09Vk/img_640x640.jpg"
+		}
+	}
+}
+</pre>
--- src/Kakao/KakaoLogin.java
+++ src/Kakao/KakaoLogin.java
@@ -1,35 +1,89 @@
 package Kakao;
 import java.io.*;
-import java.net.*;
-import javax.net.ssl.HttpsURLConnection;
+import java.util.*;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import com.google.gson.*;
+import com.google.gson.stream.*;
+import com.google.gson.internal.LinkedTreeMap;
 
 public class KakaoLogin {
-	private final String USER_AGENT = "Mozilla/5.0";
-	private final String targetUrl = "https://kauth.kakao.com/oauth/token";
 	
-	public String kakaoHttpPost(String parameters) throws Exception {
-		URL url = new URL(targetUrl);
-		HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
+	private static final String USER_AGENT = "Mozila/5.0";
+	
+	public static String getToken(String code) throws ClientProtocolException, IOException {
+		System.out.println("---카카오 로그인 시작---");
 		
-		con.setRequestMethod("POST");
-		con.setRequestProperty("User-Agent", USER_AGENT);
-		con.setDoOutput(true);
+		//http client 생성
+		CloseableHttpClient httpClient = HttpClients.createDefault();
 		
-		DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-		wr.writeBytes(parameters);
-		wr.flush();
-		wr.close();
+		//get 메서드와 URL 설정
+		HttpPost post = new HttpPost("https://kauth.kakao.com/oauth/token");
 		
-		int responseCode = con.getResponseCode();
-		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-		String inputLine;
-		StringBuffer response = new StringBuffer();
+		//agent 정보 설정
+		post.addHeader("User-Agent", USER_AGENT);
+		post.addHeader("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
 		
-		while((inputLine = in.readLine()) != null) {
-			response.append(inputLine);
-		}
-		in.close();
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+	    params.add(new BasicNameValuePair("grant_type", "authorization_code"));
+	    params.add(new BasicNameValuePair("client_id", "f4b335bfa37a8ce098ed450312b37a35"));
+	    params.add(new BasicNameValuePair("redirect_uri", "http://amel.zz.am/member/login/kakao_login.jsp"));
+	    params.add(new BasicNameValuePair("code", code));
+	    post.setEntity(new UrlEncodedFormEntity(params));
 		
-		return ("HTTP 응답 코드 : " + responseCode + "<br>HTTP body : " + response.toString());
+		//get 요청
+		CloseableHttpResponse httpResponse = httpClient.execute(post);
+		
+		System.out.println("GET Response Status");
+		System.out.println(httpResponse.getStatusLine().getStatusCode());
+		String res = EntityUtils.toString(httpResponse.getEntity(), "UTF-8");
+		
+		httpClient.close();
+		
+		System.out.println(res);	//디버그
+		System.out.println("getToken메소드 종료");
+		System.out.println("");
+		
+		return res;
+	}
+	
+	public static String getProfile(String instring) throws ClientProtocolException, IOException {
+		
+		Gson gson = new Gson();
+		Map<String,String> injson = new HashMap<String,String>();
+		injson = (Map<String,String>) gson.fromJson(instring, injson.getClass());
+		
+		//http client 생성
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+		
+		//get 메서드와 URL 설정
+		HttpPost post = new HttpPost("https://kapi.kakao.com/v2/user/me");
+		
+		//agent 정보 설정
+		post.addHeader("User-Agent", USER_AGENT);
+		post.addHeader("Authorization", "Bearer "+injson.get("access_token"));
+		post.addHeader("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+		
+		//get 요청
+		CloseableHttpResponse httpResponse = httpClient.execute(post);
+		
+		System.out.println("GET Response Status");
+		System.out.println(httpResponse.getStatusLine().getStatusCode());
+		String res = EntityUtils.toString(httpResponse.getEntity(), "UTF-8");
+		
+		httpClient.close();
+		
+		System.out.println(res);	//디버그
+		System.out.println("getProfile메소드 종료");
+		System.out.println("");
+		
+		return res;
 	}
 }
